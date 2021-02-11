0ef18290c71fc9db8cea18ddf45bef95bfb2b874
Ieva Markunaite <markunaite.ieva@gmail.com>
2021-02-11 13:29:48 +0200

    20210211 klasės darbas

---
+++ JAVASCRIPT/20210208/js/script.js
@@ -0,0 +1,98 @@
+// let a = prompt('enter');
+
+// document.write('hello');
+// document.write('<p>hello</p>');
+
+// document.getElementById('...').innerHTML = ' ';
+
+// let a = 20;
+// console.log('hello');
+// console.log(a);
+
+// console.error('error');
+// console.warn('warning');
+
+// var name = 'John';
+
+// let name = 'John';
+// let name = 'Ana';
+
+// var num1 = 20;
+// let num2 = 30;
+
+// console.log(num1);
+// console.log(num2);
+
+// const num1 = 25;
+// console.log(num1);
+// num 1 = 10 + 2;
+// console.log(num1); //neleidzia priskirti naujos reiksmes
+
+// = prsikyrimas
+// == /ar reiksme lygi
+// === ar atitinka reiksme ir tipas
+
+// const name = 'Jphn';
+// const num1 = 20;
+// const num2 = 2.8;
+// const isTall = false;
+// const x = null;
+// const y = undefined;
+// let z;
+// console.log(typeof(name));
+
+// let name = 'John';
+// let surname = 'Doe';
+// console.log(`My name is ${name}, and my surname is ${surname}`);
+
+// console.log('John' + 'Doe');
+
+// let name = 'Ieva';
+// name.length;
+// name.toUpperCase();
+// name.toLowerCase();
+// console.log(name.indexOf('v'));
+// console.log(name.slice(1,3));
+
+// let items = new Array();
+// items[0] = 'item1';
+// items[1] = 'item2';
+// console.log(items);
+
+// let items = ['item1', 'item2', 'item2', 'item4'];
+// console.log(items);
+// console.log(items[0]);
+
+// let phone = new Object();
+// phone['gamintojas'] = 'Apple';
+// phone['spalva'] = 'Baltas';
+// phone['modelis'] = 'iPhone';
+
+// console.log(phone);
+
+// let phone = {
+// gamintojas: 'Apple', 
+// spalva: 'Baltas', 
+// gamintojas: 'iPhone',
+// gps: true
+// };
+
+// console.log(phone.spalva);
+
+// let phone = [
+//     {
+//     gamintojas: 'Apple', 
+//     spalva: 'Baltas', 
+//     modelis: 'iPhone',
+//     gps: true
+//     },
+
+//     {
+//     gamintojas: 'Samsung',
+//     spalva: 'Raudona',
+//     modelis: 'galaxy',
+//     gps: true
+//     }
+// ]
+    
+//     console.log(phone.spalva);
